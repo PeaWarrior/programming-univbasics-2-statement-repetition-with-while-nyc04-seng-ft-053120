4f5c94635e488d310079fcdb0d251e5f8a2108cf
Jackson Chen <jacksonchen171@gmail.com>
2020-06-02 20:16:17 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
 current_num = 10
 while current_num >=0 do
-  puts current_num
+  puts "+#{current_num}"
   if current_num == 0
-    puts "Happy New Year!"
+    puts "+Happy New Year!"
   end
   current_num-=1
 end
